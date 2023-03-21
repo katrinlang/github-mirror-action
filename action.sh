0f396fc380345d807e75c4644449ee943aa4f314
Katrin Lang <mail@katrinlang.de>
2023-03-21 13:32:30 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -11,6 +11,6 @@ git config --global --add safe.directory /github/workspace
 
 git clone --recurse-submodules --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
+git submodule update --init --recursive
 git fetch --recurse-submodules=yes mirror +refs/heads/*:refs/remotes/origin/* || exit 1
-git submodule update
 git push --force --mirror --prune origin || exit 1
